a269e89a5cef1a2241a3af75ac45273d70267155
hdferguson <hdferguson@valdosta.edu>
2017-03-08 23:54:34 +0000

    added a link_to the new reviews page. It has to be a link_to and not a button because of post being for buttons and get being for link_to.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :reviews
  resources :papers, only: [:index, :new, :create, :destroy]
   root "papers#index"
-  
+  get "reviews/new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
